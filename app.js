cfcbad223b7a050f1e8522e5b26c4b1d315f1636
aim3pc <aaqilmohammad93@gmail.com>
2023-02-17 22:44:15 -0500

    Making updates to the sites and pulling in trending movies

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const options = {
 };
 
 const movieSearchURL = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=';
+const latestURL = 'https://api.themoviedb.org/3/movie/popular?api_key=0d33a1df54c8b2be9634e106a4020e28&language=en-US';
 
 let movieQuery = document.getElementById('movieTitle');
 
@@ -16,12 +17,27 @@ let movieSearchImage = document.getElementById('moveImg');
 let movieSearchTitle = document.getElementById('heading');
 let movieDescription = document.getElementById('movDesc');
 const movieContainer = document.getElementById('results');
+const lastestContainer = document.getElementById('latest');
+const lastestRow = document.getElementById('row');
 const reviewContainer = document.getElementById('review');
 const reviewButton = document.getElementById('sendReview');
 let reviewScore = document.getElementById('reviewScore');
 
+async function getLatest(){
+    const response = await fetch(latestURL);
+    const data = await response.json();
+    console.log(data);
+    for(i=0; i < data.results.length; i++){
+        latestFilmCard(data.results[i].original_title, data.results[i].overview, "C:/Users/aaqil/OneDrive/Desktop/No_Image_Available.jpg");
+
+    }
+}
+
+getLatest();
+
 async function searchMovie(name){
     try{
+    lastestContainer.classList.add('hidden');
     const response = await fetch(movieSearchURL + name, options);
     const data = await response.json();
     console.log(data);
@@ -39,9 +55,10 @@ async function searchMovie(name){
 movieButton.onclick = function (){
     searchMovie(movieQuery.value);
     movieQuery.value = " ";
- }
+}
 
  function displayMovie(img, title, description){
+    lastestContainer.classList.add('hidden');
     movieContainer.classList.remove('hidden');
     movieSearchImage.src = img;
     movieSearchTitle.innerText = title;
@@ -81,6 +98,34 @@ movieButton.onclick = function (){
     singleRevBody.appendChild(imdbLink);
  }
 
+ function latestFilmCard(title, synopsis, img){
+    const singleReview = document.createElement('div');
+    singleReview.classList.add('card');
+    singleReview.style.width = '18rem';
+    singleReview.style.marginLeft ='10px';
+    singleReview.style.marginTop = '10px';
+
+    const singleRevBody = document.createElement('div');
+    singleRevBody.classList.add('card-body');
+
+    const movieTitle = document.createElement('h5');
+    movieTitle.classList.add('card-title');
+    movieTitle.innerText = title;
 
 
+    const reviewAdd = document.createElement('p');
+    reviewAdd.classList.add('card-text');
+    reviewAdd.innerText = synopsis;
 
+    const movieImage = document.createElement('img');
+    movieImage.src = img;
+    movieImage.classList.add('card-img-top');
+    
+
+    lastestContainer.appendChild(singleReview);
+    singleRevBody.appendChild(movieImage);
+    singleReview.appendChild(singleRevBody);
+    singleRevBody.appendChild(movieTitle);
+    singleRevBody.appendChild(reviewAdd)
+    
+ }
